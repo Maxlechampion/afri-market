b4bff494092f901d566f937b1bbfa98dbf754c09
Emile <taho@vippinterstis.com>
2026-01-22 19:24:00 +0100

    corretion1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    // On définit uniquement les clés nécessaires en les transformant en chaînes de caractères
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env.FEDAPAY_PUBLIC_KEY': JSON.stringify(process.env.FEDAPAY_PUBLIC_KEY),
   }
 });
